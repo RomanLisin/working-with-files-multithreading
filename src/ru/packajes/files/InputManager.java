0d2d0a0d59751ddc7712ec8fcbe485c7d3981539
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2026-01-04 23:28:18 +1000

    InputManager

---
+++ src/ru/packajes/files/InputManager.java
@@ -0,0 +1,34 @@
+package ru.packajes.files;
+
+import java.util.Scanner;
+
+public class InputManager {
+    private final Scanner scanner;
+
+
+    public InputManager() {
+        this.scanner = new Scanner(System.in);
+
+    }
+
+    public String getInput(String text){
+        System.out.println(text);
+        return scanner.nextLine();
+    }
+
+    public int[] getIntArray(String text){
+        System.out.println(text);
+        String input = scanner.nextLine();
+        String[] parts = input.trim().split("\\s+");
+        int[] array = new int[parts.length];
+
+        for (int i=0;i<parts.length;i++){
+            array[i] = Integer.parseInt(parts[i]);
+        }
+        return array;
+    }
+
+    public void close(){
+        scanner.close();
+    }
+}
--- src/ru/packajes/files/Main.java
+++ src/ru/packajes/files/Main.java
@@ -1,10 +1,17 @@
 package ru.packajes.files;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
+import ru.packajes.files.FileManager;
+import ru.packajes.files.InputManager;
 
 public class Main {
 
-    void main() {
-            Scanner input = new Scanner(System.in);
+    public static void main(String[] args){
+        //Scanner input = new Scanner(System.in);
+        InputManager inputManager = new InputManager();
+        FileManager fileManager = new FileManager();
 
         System.out.println("Работа с файлами");
         System.out.println("1. Сравнение строк двух файлов");
@@ -14,63 +21,68 @@ public class Main {
         System.out.println("5. Информационная система 'Корпорация'");
         System.out.println("Выберите задание (1-5): ");
 
-        int choice = input.nextLine();
+        //int choice = input.nextLine();
+        String choice = inputManager.getInput("");
 
-        switch (choice){
-            case 1:
-                task1();
-                break;
-            case 2:
-                task2();
-                break;
-            case 3:
-                task3();
-                break;
-            case 4:
-                task4();
-                break;
-            case 5:
-                task5();
-                break;
-            default:
-                System.out.println("Неверный выбор!");
+        try {
+            switch (choice) {
+                case "1":
+                    task1(inputManager, fileManager);
+                    break;
+                case "2":
+                    task2(inputManager, fileManager);
+                    break;
+                case "3":
+                    task3(inputManager, fileManager);
+                    break;
+                case "4":
+                    task4(inputManager, fileManager);
+                    break;
+                case "5":
+                    task5();
+                    break;
+                default:
+                    System.out.println("Неверный выбор!");
 
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка работы с файлом: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка формата числа: " + e.getMessage());
         }
-
-        private static void task1(){
+        inputManager.close();
+    }
+        private static void task1(InputManager input, FileManager fileManager) throws IOException {
             System.out.println("Задание 1: Сравнение строк двух файлов");
-            System.out.println("Введите путь к первому файлу: ");
-            String filePath1 = input.nextLine();
-            System.out.println("Введите путь ко второму файлу: ");
-            String filePath2 = input.nextLine();
-        }
-        private static void task2(){
+            String filePath1 = input.getInput("Введите путь к первому файлу: ");
+            String filePath2 =input.getInput("Введите путь ко второму файлу: ");
+            List<String> lines1 = fileManager.readAllLines(filePath1);
+            List<String> lines2 = fileManager.readAllLines(filePath2);
+
+          }
+
+        private static void task2(InputManager input, FileManager fileManager){
             System.out.println("Задание 2: Поиск самой длинной строки");
-            System.out.println("Введите путь к файлу: ");
-            String filePath = input.nextLine();
+            String filePath = input.getInput("Введите путь к файлу: ");
         }
 
-        private static void task3(){
+        private static void task3(InputManager input, FileManager fileManager){
             System.out.println("Задание 3: Обработка массивов из файлов");
-            System.out.println("Введите путь к файлу: ");
-            String filePath = input.nextLine();
+            String filePath = input.getInput("Введите путь к файлу: ");
+
         }
         }
 
-        private static void task4(){
+        private static void task4(InputManager input, FileManager fileManager){
             System.out.println("Задание 4: Сохранение массива в файл");
-            System.out.println("Введите путь к файлу: ");
-            String filePath1 = input.nextLine();
-            System.out.println("Введите элементы массива через пробел");
-            String inputNum = input.nextLine();
-            String[] parts = inputNum.split(" ");
-            int[] array = new int[parts.length];
-            for(int i =0; i<parts.length; i++){
-                array[i]= Integer.parseInt(parts[i]);
+            String filePath = input.getInput("Введите путь к файлу: ");
+            int[] inputNum = input.getIntArray("Введите элементы массива через пробел: ");
+
+
         }
 
         private static void task5(){
 
         }
-    }
 }
+
